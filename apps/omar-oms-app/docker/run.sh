a99af3bc11eb636784e666cbbec29021f59d2005
Garrett Potts <gpotts@radiantblue.com>
2017-07-24 10:21:58 -0400

    Added docker componenets to test riofs

---
+++ apps/omar-oms-app/docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -z $AWS_ACCESS_KEY ] ; then
+  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+fi
+mkdir -p /mnt/s3/$BUCKET
+echo "BUCKET = ${BUCKET}"
+riofs -o allow_other -c /usr/share/omar/riofs.conf.xml  $BUCKET /mnt/s3/$BUCKET
+
+java -server -Xms256m -Xmx1024m -Djava.awt.headless=true -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -Djava.security.egd=file:/dev/./urandom -jar /usr/share/omar/omar-oms-app.jar"
