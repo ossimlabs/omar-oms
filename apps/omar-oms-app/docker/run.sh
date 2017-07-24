71eec4faa3fa4b8a45875b1fd9bccd29143f77ce
Garrett Potts <gpotts@radiantblue.com>
2017-07-24 14:01:55 -0400

    Testing not running the riofs

--- apps/omar-oms-app/docker/run.sh
+++ apps/omar-oms-app/docker/run.sh
@@ -5,6 +5,6 @@ if [ ! -z $AWS_ACCESS_KEY ] ; then
 fi
 mkdir -p /s3/$BUCKET
 echo "BUCKET = ${BUCKET}"
-riofs -o allow_other -c /usr/share/omar/riofs.conf.xml  $BUCKET /s3/$BUCKET
+#riofs -o allow_other -c /usr/share/omar/riofs.conf.xml  $BUCKET /s3/$BUCKET
 export JAR_FILE=`find /usr/share/omar -name "*.jar"`
 java -server -Xms256m -Xmx1024m -Djava.awt.headless=true -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -Djava.security.egd=file:/dev/./urandom -jar $JAR_FILE
