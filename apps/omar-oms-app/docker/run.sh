43ae272a24328563d3f457d1ab7e38050aae5067
Garrett Potts <gpotts@radiantblue.com>
2017-07-24 14:12:09 -0400

    Removed mount call

--- apps/omar-oms-app/docker/run.sh
+++ apps/omar-oms-app/docker/run.sh
@@ -4,7 +4,7 @@ if [ ! -z $AWS_ACCESS_KEY ] ; then
   export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 fi
 mkdir -p /s3/$BUCKET
-echo "BUCKET = ${BUCKET}"
+#echo "BUCKET = ${BUCKET}"
 #riofs -o allow_other -c /usr/share/omar/riofs.conf.xml  $BUCKET /s3/$BUCKET
 export JAR_FILE=`find /usr/share/omar -name "*.jar"`
 java -server -Xms256m -Xmx1024m -Djava.awt.headless=true -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -Djava.security.egd=file:/dev/./urandom -jar $JAR_FILE
