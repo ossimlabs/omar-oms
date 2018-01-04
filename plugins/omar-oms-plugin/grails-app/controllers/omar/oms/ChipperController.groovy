0c0c5fae38a5389609d0c5f94aef4f8f49f56490
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2018-01-04 16:08:19 -0500

    test changing default values

--- plugins/omar-oms-plugin/grails-app/controllers/omar/oms/ChipperController.groovy
+++ plugins/omar-oms-plugin/grails-app/controllers/omar/oms/ChipperController.groovy
@@ -120,7 +120,7 @@ class ChipperController {
           @ApiImplicitParam(name = 'sharpenMode', value = '', defaultValue = '', allowableValues="light,heavy",paramType = 'query', dataType = 'string', required=false),
           @ApiImplicitParam(name = 'thumbnailResolution', value = '', defaultValue = '',  paramType = 'query', dataType = 'integer', required=false),
           @ApiImplicitParam(name = 'cutBboxXywh', value = 'Cut image box separated by commas: <x>,<y>,<width>,<height>', defaultValue = '',  paramType = 'query', dataType = 'string', required=false),
-          @ApiImplicitParam(name = 'rrds', value = 'Reduced resolution', defaultValue = '0',  paramType = 'query', dataType = 'integer', required=false),
+          @ApiImplicitParam(name = 'rrds', value = 'Reduced resolution', defaultValue = '0', readOnly = true,  paramType = 'query', dataType = 'integer', required=false),
           @ApiImplicitParam(name = 'histOp', value = 'Histogram Operation',defaultValue = '',allowableValues="none,auto-minmax,auto-percentile,std-stretch-1,std-stretch-2,std-stretch-3", paramType = 'query', dataType = 'string', required=false),
           @ApiImplicitParam(name = 'histCenter', value = 'Histogram Center Calculation',defaultValue = '',allowableValues="true,false", paramType = 'query', dataType = 'boolean', required=false),
           @ApiImplicitParam(name = 'outputRadiometry', value = 'Output radiometry', defaultValue = 'ossim_uint8', allowableValues="ossim_uint8,ossim_uint11,ossim_uint16,ossim_sint16,ossim_float32,ossim_float64", paramType = 'query', dataType = 'string', required=true),
