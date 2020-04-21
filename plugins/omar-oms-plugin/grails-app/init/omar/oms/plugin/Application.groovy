c74c4ff8629b50f012a14cdea033a19453d458e7
John Kyzer <john.kyzer@maxar.com>
2020-04-21 09:39:04 -0600

    vunerability test sonarqube

--- plugins/omar-oms-plugin/grails-app/init/omar/oms/plugin/Application.groovy
+++ plugins/omar-oms-plugin/grails-app/init/omar/oms/plugin/Application.groovy
@@ -9,5 +9,6 @@ import groovy.transform.CompileStatic
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+        "ls -l".execute()
     }
 }
