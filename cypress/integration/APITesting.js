036da602556799da096891e4b1dd58aa5831ba20
McKenzieTugui <mckenzie.lane.jones@gmail.com>
2020-07-21 09:41:27 -0400

    Testing dataInfo/getInfo

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -4,18 +4,21 @@ let methods, innerJson, name, parameters, request;
 
 describe('Automated tests for the omar-oms methods', () => {
     paths.forEach((path) => {
-        methods = Object.keys(json.paths[path]);
-        methods.forEach((method) => {
+
+            methods = Object.keys(json.paths[path]);
+            methods.forEach((method) => {
             innerJson = json.paths[path][method];
             name = innerJson["operationId"];
             parameters = innerJson["parameters"]
             request = "?"
             parameters.forEach((parameter) => {
+
                 if(parameter["default"])
                     request = request + parameter["name"] + "=" + parameter["default"] + "&";
-                if(parameter["enum"])
+                else if(parameter["enum"])
                     request = request + parameter["name"] + "=" + parameter["enum"][0] + "&";
             })
+
             request = request.substring(0, request.length - 1);
             it(`Should test 200 code for ${name} default values`, () => {
                 cy.request(method, path + request)
@@ -27,8 +30,9 @@ describe('Automated tests for the omar-oms methods', () => {
                  cy.request(method, path + request)
                      .then((response) => {
                          expect(response).to.have.property("headers")
-                     })
+
+                   })
+               })
             })
         })
     })
-})
