2f5da5ef710d7d2185dc9c25879f552d7e3c1ed0
aimee <aimee@localhost.localdomain>
2020-07-24 10:22:07 -0400

    Set up example test.js file

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -1,38 +1,15 @@
-let json = require('../../plugins/omar-oms-plugin/build/swaggerSpec.json');
-let paths = Object.keys(json.paths);
-let methods, innerJson, name, parameters, request;
-
 describe('Automated tests for the omar-oms methods', () => {
-    paths.forEach((path) => {
-
-            methods = Object.keys(json.paths[path]);
-            methods.forEach((method) => {
-            innerJson = json.paths[path][method];
-            name = innerJson["operationId"];
-            parameters = innerJson["parameters"]
-            request = "?"
-            parameters.forEach((parameter) => {
-
-                if(parameter["default"])
-                    request = request + parameter["name"] + "=" + parameter["default"] + "&";
-                else if(parameter["enum"])
-                    request = request + parameter["name"] + "=" + parameter["enum"][0] + "&";
-            })
-
-            request = request.substring(0, request.length - 1);
-            it(`Should test 200 code for ${name} default values`, () => {
-                cy.request(method, path + request)
+            it(`Should test 200 code for getInfo default values`, () => {
+                cy.request("get", "/dataInfo/getInfo?filename=%2Fdata%2Fs3%2Fadhoc%2F16SEP08110841-M1BS-055998376010_01_P006.TIF&entry=0")
                     .then((response) => {
                         expect(response.status).to.eq(200)
                     })
             })
-            it(`Should test response header for ${name}`, () => {
-                 cy.request(method, path + request)
+            it(`Should test response header for getInfo`, () => {
+                 cy.request("get", "/dataInfo/getInfo?filename=%2Fdata%2Fs3%2Fadhoc%2F16SEP08110841-M1BS-055998376010_01_P006.TIF&entry=0")
                      .then((response) => {
                          expect(response).to.have.property("headers")
 
                    })
                })
-            })
-        })
     })
