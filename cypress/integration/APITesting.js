d507a5ec9753fc1f061a8ad5ab0d29d26c877a57
aimee <aimee@localhost.localdomain>
2020-07-28 08:22:49 -0400

    Added Test Configuration file

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -1,15 +1,32 @@
-describe('Automated tests for the omar-oms methods', () => {
-            it(`Should test 200 code for getInfo default values`, () => {
-                cy.request("get", "/dataInfo/getInfo?filename=%2Fdata%2Fs3%2Fadhoc%2F16SEP08110841-M1BS-055998376010_01_P006.TIF&entry=0")
+let json = require('../../testParameters.json');
+let tests = Object.keys(json.tests);
+let innerJson, method, endpoint, query, parameters, request, keys;
+
+describe('Automated tests for the omar-oms API endpoints', () => {
+    tests.forEach((test) => {
+        innerJson = json.tests[test];
+        method = innerJson["method"];
+        endpoint = innerJson["endpoint"];
+        query = innerJson["in"] === "query";
+        parameters = Object.keys(innerJson["parameters"]);
+        if(query) {
+            request = "?"
+            parameters.forEach((parameter) => {
+                request = request + parameter + "=" + innerJson.parameters[parameter] + "&";
+            })
+            request = request.substring(0, request.length - 1);
+            it(`Should test 200 code for ${test} default values`, () => {
+                cy.request(method, endpoint + request)
                     .then((response) => {
                         expect(response.status).to.eq(200)
                     })
             })
-            it(`Should test response header for getInfo`, () => {
-                 cy.request("get", "/dataInfo/getInfo?filename=%2Fdata%2Fs3%2Fadhoc%2F16SEP08110841-M1BS-055998376010_01_P006.TIF&entry=0")
-                     .then((response) => {
-                         expect(response).to.have.property("headers")
-
-                   })
-               })
+            it(`Should test response header for ${test}`, () => {
+                cy.request(method, endpoint + request)
+                    .then((response) => {
+                        expect(response).to.have.property("headers")
+                    })
+            })
+        }
     })
+})
